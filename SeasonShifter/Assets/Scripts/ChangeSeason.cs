418348557ba3b66d1ee5fd92d27d3ea8b9de7654
fbHolzbaum <blancofel22@gmail.com>
2016-04-16 11:21:36 +0200

    Did some base stuff

---
+++ SeasonShifter/Assets/Scripts/ChangeSeason.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSeason : MonoBehaviour {
+
+    enum Season { summer, winter};
+    Season currentSeason = Season.winter;
+
+    GameObject summerObject;
+    GameObject winterObject;
+
+    public GameObject changeSeasonEffect;
+
+    void Start()
+    {
+        summerObject = GameObject.FindGameObjectWithTag("Summer");
+        winterObject = GameObject.FindGameObjectWithTag("Winter");
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    if(Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine(ChangeEffect());
+        }
+	}
+
+
+    IEnumerator ChangeEffect()
+    {
+        GameObject changeEffect = Instantiate(changeSeasonEffect, transform.position, transform.rotation) as GameObject;
+        yield return new WaitForSeconds(0.9f);
+        if (currentSeason == Season.summer)
+        {
+            currentSeason = Season.winter;
+            summerObject.SetActive(false);
+            winterObject.SetActive(true);
+        }
+        else
+        {
+            currentSeason = Season.summer;
+            summerObject.SetActive(true);
+            winterObject.SetActive(false);
+        }
+        yield return new WaitForSeconds(0.2f);
+        Destroy(changeEffect);
+    }
+}
