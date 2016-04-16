5ccd72b575c2626460c3a87f0bb6e12bc6ec654c
fbHolzbaum <blancofel22@gmail.com>
2016-04-16 20:24:59 +0200

    Created further animation

--- SeasonShifter/Assets/Scripts/PlayerMovement.cs
+++ SeasonShifter/Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,18 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float maxSpeed = 10;
-    public float jumpingPower = 20;
+    public float jumpingPower = 40;
+    public Transform feetCollider;
 
     private bool grounded; //if the player touches the ground
+    private bool jumping = false;
     private bool airControl = true; //if the character can be controlled in air
     private Animator playerAnimator;
     private Rigidbody2D rigidbody;
     private bool facingRight = true; //If the character is looking right or left
-    
+
+
+    private RaycastHit hit;
 
     void Start()
     {
@@ -23,6 +27,26 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Check if the character is touching the ground
+        if (Physics2D.Raycast(feetCollider.position,-Vector2.up,0.2f) && !jumping)
+        {
+            Debug.Log("Ground hit");
+            grounded = true;
+        }
+        else
+        {
+            Debug.Log("Ground not hit");
+            grounded = false;
+        }
+
+        if(!grounded)
+        {
+            playerAnimator.SetBool("Grounded", false);
+        }
+        else
+        {
+            playerAnimator.SetBool("Grounded", true);
+        }
     }
 
     public void Move(float move, bool jump)
@@ -35,7 +59,17 @@ public class PlayerMovement : MonoBehaviour
             // Move the character
             rigidbody.velocity = new Vector2(move * maxSpeed, rigidbody.velocity.y);
 
-            if(move > 0 && !facingRight)
+            if (jump)
+            {
+                playerAnimator.SetBool("Jump", true);
+                rigidbody.AddForce(new Vector2(0f, jumpingPower));
+            }
+            else
+            {
+                playerAnimator.SetBool("Jump", false);
+            }
+
+            if (move > 0 && !facingRight)
             {
                 Flip();
             }
@@ -56,6 +90,12 @@ public class PlayerMovement : MonoBehaviour
         theScale.x *= -1;
         transform.localScale = theScale;
     }
+
+    public bool ReturnGroundedState()
+    {
+        return grounded;
+    }
+
 }
 
 
