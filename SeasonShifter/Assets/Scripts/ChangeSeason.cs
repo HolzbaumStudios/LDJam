01da17755a412bdba1996ee81439fbaf317fd107
fbHolzbaum <blancofel22@gmail.com>
2016-04-17 16:45:04 +0200

    Added Staff functionality

--- SeasonShifter/Assets/Scripts/ChangeSeason.cs
+++ SeasonShifter/Assets/Scripts/ChangeSeason.cs
@@ -8,6 +8,7 @@ public class ChangeSeason : MonoBehaviour {
 
     GameObject summerObject;
     GameObject winterObject;
+    GameObject player;
 
     public Transform originObject;
     public GameObject changeSeasonEffect;
@@ -20,22 +21,29 @@ public class ChangeSeason : MonoBehaviour {
         winterObject = GameObject.FindGameObjectWithTag("Winter");
         if (currentSeason == Season.winter) { summerObject.SetActive(false); } else { winterObject.SetActive(false); }
         if (PlayerPrefs.GetInt("StaffEnabled") != 1) changeAllowed = false;
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update () {
-	    if(Input.GetButtonDown("Fire1") && changeAllowed)
+	    if(Input.GetButtonDown("Fire1") && changeAllowed && GetPlayerState())
         {
-            StartCoroutine(ChangeEffect());
+            SeasonChange();
         }
 	}
+
+    //Starts the change effect coroutine. This function is also called from other scripts
+    public void SeasonChange()
+    {
+        StartCoroutine(ChangeEffect());
+    }
 
 
     IEnumerator ChangeEffect()
     {
         Vector3 instantiatePosition = new Vector3(originObject.position.x, originObject.position.y, 1);
         GameObject changeEffect = Instantiate(changeSeasonEffect, instantiatePosition, transform.rotation) as GameObject;
-        yield return new WaitForSeconds(0.9f);
+        yield return new WaitForSeconds(0.5f);
         if (currentSeason == Season.summer)
         {
             currentSeason = Season.winter;
@@ -61,5 +69,12 @@ public class ChangeSeason : MonoBehaviour {
     public void AllowChange(bool allowed)
     {
         changeAllowed = allowed;
+    }
+
+    //Ask player movement if the player may change the season
+    public bool GetPlayerState()
+    {
+        bool playerState = player.GetComponent<PlayerMovement>().ReturnPlayerState();
+        return playerState;
     }
 }
