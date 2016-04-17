f913cf92003923e74d2541ded7d5191c5a8b3b2e
fbHolzbaum <blancofel22@gmail.com>
2016-04-17 19:10:17 +0200

    Added Water Jump Zones

---
+++ SeasonShifter/Assets/Scripts/Water/WaterJump.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterJump : MonoBehaviour {
+
+    GameObject player;
+    PlayerMovement playerMovementScript;
+    public float outOfWaterJumpPower; //Power to jump out of the water
+    private float initialJumpingPower;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerMovementScript = player.GetComponent<PlayerMovement>();
+        
+    }
+
+   void OnTriggerStay2D(Collider2D col)
+    {
+        if(Input.GetButtonDown("Jump") && col.CompareTag("Player"))
+        {
+            col.transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, outOfWaterJumpPower));
+           // playerMovementScript.ChangeSwimmingState(false);
+        }
+    }
+
+}
