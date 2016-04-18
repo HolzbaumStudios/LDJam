bb76daf8388c463bf545a0585b459d3908fe5a7a
SirWoody <roman.martinello@gmail.com>
2016-04-18 11:27:43 +0200

    Added Plattform movement to Map2

--- SeasonShifter/Assets/Scripts/PlattformMovement.cs
+++ SeasonShifter/Assets/Scripts/PlattformMovement.cs
@@ -5,9 +5,12 @@ public class PlattformMovement : MonoBehaviour {
 
     // Variables
     private Vector2 originPosition;
+    public float maxDistance;
+    //private float maxDistance = 2.0f;
+    public bool sideways = false;
     public bool movingdown; //to check if the plattform reached maxDistance -> Switch in the opposing direction
+    public bool movingright; //to check if the plattform reached maxDistance -> Switch in the opposing direction
     private float movementSpeed;
-    private float maxDistance = 2.0f;
     private ChangeSeason seasonManager;
 
     //Season Sprites
@@ -29,27 +32,55 @@ public class PlattformMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        int seasonNumber = seasonManager.GetSeason();
-        if((Season)seasonNumber != currentSeason)
+        if (sideways == false) // Wenn die Seitwärtsbewegung ausgeschaltet ist
         {
-            currentSeason = (Season)seasonNumber;
-            ChangeSprite();
+            int seasonNumber = seasonManager.GetSeason();
+            if ((Season)seasonNumber != currentSeason)
+            {
+                currentSeason = (Season)seasonNumber;
+                ChangeSprite();
+            }
+            float moveY = 1.0f;
+            if (movingdown == true) moveY *= -1;
+            if (currentSeason == Season.summer)
+            {
+                movementSpeed = 2.0f;
+                transform.Translate(new Vector2(0, moveY) * movementSpeed * Time.deltaTime);
+                if (originPosition.y - maxDistance > transform.position.y) movingdown = false;
+                if (originPosition.y + maxDistance < transform.position.y) movingdown = true;
+            }
+            else if (currentSeason == Season.winter)
+            {
+                movementSpeed = 0.0f;
+                transform.Translate(new Vector2(0, moveY) * movementSpeed * Time.deltaTime);
+                if (originPosition.y - maxDistance > transform.position.y) movingdown = false;
+                if (originPosition.y + maxDistance < transform.position.y) movingdown = true;
+            }
         }
-        float moveY = 1.0f;
-        if (movingdown == true) moveY *= -1;
-        if (currentSeason == Season.summer)
+        else //Wenn Seitwärts true -> Bewegung links-rechts
         {
-            movementSpeed = 2.0f;
-            transform.Translate(new Vector2(0, moveY) * movementSpeed * Time.deltaTime);
-            if (originPosition.y - maxDistance > transform.position.y) movingdown = false;
-            if (originPosition.y + maxDistance < transform.position.y) movingdown = true;
-        }
-        else if (currentSeason == Season.winter)
-        {
-            movementSpeed = 0.0f;
-            transform.Translate(new Vector2(0, moveY) * movementSpeed * Time.deltaTime);
-            if (originPosition.y - maxDistance > transform.position.y) movingdown = false;
-            if (originPosition.y + maxDistance < transform.position.y) movingdown = true;
+            int seasonNumber = seasonManager.GetSeason();
+            if ((Season)seasonNumber != currentSeason)
+            {
+                currentSeason = (Season)seasonNumber;
+                ChangeSprite();
+            }
+            float moveX = 1.0f;
+            if (movingright == false) moveX *= -1;
+            if (currentSeason == Season.summer)
+            {
+                movementSpeed = 2.0f;
+                transform.Translate(new Vector2(moveX, 0) * movementSpeed * Time.deltaTime);
+                if (originPosition.x - maxDistance > transform.position.x) movingright = true;
+                if (originPosition.x + maxDistance < transform.position.x) movingright = false;
+            }
+            else if (currentSeason == Season.winter)
+            {
+                movementSpeed = 0.0f;
+                transform.Translate(new Vector2(moveX, 0) * movementSpeed * Time.deltaTime);
+                if (originPosition.x - maxDistance > transform.position.x) movingright = true;
+                if (originPosition.x + maxDistance < transform.position.x) movingright = false;
+            }
         }
 	}
 
