6b9e3280a71d9ca5c46c1bd82875d6c1e34130b5
fbHolzbaum <blancofel22@gmail.com>
2016-04-18 11:34:47 +0200

    Finished Background

---
+++ SeasonShifter/Assets/Scripts/ChangeBackground.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChangeBackground : MonoBehaviour {
+
+    public enum Season { spring, summer, fall, winter };
+    Season currentSeason;
+    ChangeSeason seasonManager;
+    Image spriteRenderer;
+    public Sprite[] backgroundImage;
+
+    // Use this for initialization
+    void Start () {
+        seasonManager = GameObject.Find("GameManager").GetComponent<ChangeSeason>();
+        spriteRenderer = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int seasonNumber = seasonManager.GetSeason();
+        if (currentSeason != (Season)seasonNumber)
+        {
+            currentSeason = (Season)seasonNumber;
+            ChangeSprite();
+        }
+    }
+
+    void ChangeSprite()
+    {
+        switch(currentSeason)
+        {
+            case Season.spring: spriteRenderer.sprite = backgroundImage[0]; break;
+            case Season.summer: spriteRenderer.sprite = backgroundImage[1]; break;
+            case Season.fall: spriteRenderer.sprite = backgroundImage[2]; break;
+            case Season.winter: spriteRenderer.sprite = backgroundImage[3]; break;
+        }
+
+    }
+}
