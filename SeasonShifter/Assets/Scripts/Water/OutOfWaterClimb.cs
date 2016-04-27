60b3ac8a552ebb022161eeeec0e6d2c09f3bf49a
fbHolzbaum <blancofel22@gmail.com>
2016-04-27 23:35:46 +0200

    Started Out of water movement

---
+++ SeasonShifter/Assets/Scripts/Water/OutOfWaterClimb.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutOfWaterClimb : MonoBehaviour {
+
+    public bool leftSide = true; //Should be true if exit on the left side, false if exit on the right side
+    Animator playerAnimator;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        playerAnimator = col.GetComponent<Animator>();
+    }
+
+	void OnTriggerStay2D(Collider2D col)
+    {
+        float xMovement = Input.GetAxis("Horizontal");
+        if((xMovement < -0.1f && leftSide) || (xMovement > 0.1f && !leftSide))
+        {
+            playerAnimator.SetBool("OutOfWater", true);
+        }
+            
+
+        
+    }
+}
