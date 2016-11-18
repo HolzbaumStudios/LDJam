498dad004f1a38efdf867c82ea9b6edc2f006e83
fbHolzbaum <blancofel22@gmail.com>
2016-11-18 20:45:01 +0100

    Reorganized Scenes and continued work on saves

--- SeasonShifter/Assets/Scripts/Manager/GameProgress.cs
+++ SeasonShifter/Assets/Scripts/Manager/GameProgress.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class GameProgress : MonoBehaviour {
 
     ///////////////////////////////////////////////
     //This script takes care of the game progress//
     ///////////////////////////////////////////////
+
+    public enum SaveSlots { NONE, SAVE1, SAVE2, SAVE3 }
+    private SaveSlots activeSlot = SaveSlots.NONE;
 
     //Activated Seasons
     public bool winterSeason = false;
@@ -14,7 +18,18 @@ public class GameProgress : MonoBehaviour {
 
     //Activated Abilities
     public bool umbrella = false;
-
     
+
+
+    //----------------Methods-----------------
+    public int GetSaveSlot()
+    {
+        return (int)activeSlot;
+    }
+
+    public void SetSaveSlot(int id)
+    {
+        activeSlot = (SaveSlots)id;
+    }
 
 }
---
+++ SeasonShifter/Assets/Scripts/Options/SaveHandler.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveHandler : MonoBehaviour {
+
+    GameProgress[] saveObject = new GameProgress[3];
+
+    public void Awake()
+    {
+        if (!SaveUtil.IsDirectoryExisting())
+        {
+            SaveUtil.CreateSaveDirectory();
+        }
+        LoadAllSaves();
+    }
+
+    /**
+    *
+    *This method set the GameProgress from the selected save slot as the loaded one
+    */
+	public void Load(int saveState)
+	{
+        string completePath = SaveUtil.GetPath() + SaveUtil.GetFileName(saveState);
+        GameProgress selectedSave = saveObject[saveState - 1];
+        if(selectedSave.GetSaveSlot() == 0)
+        {
+            selectedSave.SetSaveSlot(saveState);
+        }
+        GameManager.gameProgressInstance = selectedSave;
+	}
+
+	private void LoadAllSaves()
+	{
+        string path = SaveUtil.GetPath();
+        for (int i = 1; i <= 3; i++)
+        {
+            string filePath = path + SaveUtil.GetFileName(i);
+            saveObject[i - 1] = new GameProgress();
+            if(SaveUtil.IsFileExisting(filePath))
+            {
+                //Load the save to the temp objects
+            }
+        }
+	}
+}
